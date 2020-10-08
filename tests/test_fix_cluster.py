1509ebb8912815852050869f467ed6d2a9b484fe
ma2sql <fireofgod@hanmail.net>
2020-10-08 23:23:22 +0900

    test for fix cluster

--- tests/test_fix_cluster.py
+++ tests/test_fix_cluster.py
@@ -1,17 +1,22 @@
 import unittest
-from redis_trib import ClusterNode, group_by
+from redis_trib.cluster_node import Node
 
 ## TODOs
 # - open slot에 대한 판단 검증
 
-class TestUtil(unittest.TestCase):
+class TestFixCluster(unittest.TestCase):
     def setUp(self):
-        pass
+        self._node1 = Node('192.168.56.101:6379')
+        self._node1._migrating = {'1': 'abc'}
+        self._node2 = Node('192.168.56.102:6379')
+        self._node2._importing = {'1': 'efg'}
 
     # - open slot에 대한 주인 찾기
-    def testGetSlotOnwers(self):
-        # 동일 슬롯을 소유하는 오너를 2개 이상 만들기
-        assertEqual(expected, get_slot_owners())
+    def testOpenedSlots(self):
+        self.assertIn('1', self._node1.migrating.keys())
+        self.assertIn('1', self._node2.importing.keys())
+        self.assertNotIn('2', self._node1.migrating.keys())
+        self.assertNotIn('2', self._node2.importing.keys())
 
 
     def tearDown(self):
