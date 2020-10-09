a80372d0d06cc9c8c86a059b2845469d27d6ec2b
ma2sql <fireofgod@hanmail.net>
2020-10-09 21:05:30 +0900

    test for fix cluster

--- tests/test_fix_cluster.py
+++ tests/test_fix_cluster.py
@@ -1,5 +1,6 @@
 import unittest
 from redis_trib.cluster_node import Node
+from redis_trib.trib import RedisTrib
 
 ## TODOs
 # - open slot에 대한 판단 검증
@@ -17,6 +18,12 @@ class TestFixCluster(unittest.TestCase):
         self.assertIn('1', self._node2.importing.keys())
         self.assertNotIn('2', self._node1.migrating.keys())
         self.assertNotIn('2', self._node2.importing.keys())
+
+
+    def testCheckOpenSlots(self):
+        trib = RedisTrib([self._node1, self._node2])
+        open_slots = trib._check_open_slots()
+        self.assertSetEqual(open_slots, {'1'})
 
 
     def tearDown(self):
